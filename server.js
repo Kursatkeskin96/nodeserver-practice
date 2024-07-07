bc4bdd5edda6e7ce073b4f1ce09f6396b1f9190c
Kursatkeskin96 <kursatkeskinn@gmail.com>
2024-07-07 14:42:14 +0100

    vercel json changed again

--- server.js
+++ server.js
@@ -1,28 +1,33 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const mongoose = require('mongoose');
-const Product = require('./models/product.model');
-const productRoute = require('./routes/product.route');
-const cors = require("cors"); 
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const productRoute = require("./routes/product.route");
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+// Log incoming requests
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 // Routes
-app.use('/api/products', productRoute);
+app.use("/api/products", productRoute);
 
-app.listen(8000, () => console.log('Server is running on port 8000'));
-
+// MongoDB Connection
 mongoose
   .connect(process.env.MONGODB)
   .then(() => {
-    console.log('Connected to database!');
+    console.log("Connected to database!");
   })
   .catch((err) => {
-    console.error('Connection failed:', err);
+    console.error("Connection failed:", err);
   });
 
-  // Export the app for Vercel
+// Export the app for Vercel
 module.exports = app;
