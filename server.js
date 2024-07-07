e7b6b9994ef13242ea0222fffd7a3723f723c73c
Kursatkeskin96 <kursatkeskinn@gmail.com>
2024-07-07 14:34:25 +0100

    vercel added

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Product = require('./models/product.model');
 const productRoute = require('./routes/product.route');
+const cors = require("cors"); 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use('/api/products', productRoute);
